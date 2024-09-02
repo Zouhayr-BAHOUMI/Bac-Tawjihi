635c617250a9c86174a5cd47228449a202f2d5e5
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-09-02 16:45:11 +0100

    implimenter application config

--- src/main/java/tawjih/repository/PersonneRepository.java
+++ src/main/java/tawjih/repository/PersonneRepository.java
@@ -1,4 +1,14 @@
 package tawjih.repository;
 
-public interface PersonneRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import tawjih.enums.Role;
+import tawjih.model.Personne;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PersonneRepository extends JpaRepository<Personne, Integer> {
+    Optional<Personne> findByEmail(String email);
+
+    List<Personne> findByRole(Role role);
 }
