7e087c9ee12d718abc69dd7491c84170d706b381
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-09-05 20:51:38 +0100

    make service for etudiant complete his profile

--- src/main/java/tawjih/repository/AdresseRepository.java
+++ src/main/java/tawjih/repository/AdresseRepository.java
@@ -1,7 +1,13 @@
 package tawjih.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import tawjih.enums.Province;
+import tawjih.enums.Region;
+import tawjih.enums.Ville;
 import tawjih.model.Adresse;
 
+import java.util.Optional;
+
 public interface AdresseRepository extends JpaRepository<Adresse, Long> {
+    Optional<Adresse> findByRegionAndAndProvinceAndAndVille(Region region, Province province, Ville ville);
 }
--- src/main/java/tawjih/service/implimentation/EtudiantService.java
+++ src/main/java/tawjih/service/implimentation/EtudiantService.java
@@ -4,8 +4,12 @@ package tawjih.service.implimentation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tawjih.exception.EtudiantNotFoundException;
+import tawjih.model.Adresse;
 import tawjih.model.Etudiant;
+import tawjih.repository.AdresseRepository;
 import tawjih.repository.EtudiantRepository;
+
+import java.util.Optional;
 
 @Service
 public class EtudiantService {
@@ -13,11 +17,47 @@ public class EtudiantService {
     @Autowired
     private EtudiantRepository etudiantRepository;
 
+    @Autowired
+    private AdresseRepository adresseRepository;
+
     public Etudiant getEtudiant(Integer idEtudiant) {
 
         Etudiant etudiant = etudiantRepository
                 .findById(idEtudiant)
                 .orElseThrow(EtudiantNotFoundException::new);
         return etudiant;
+    }
+
+    public Etudiant updateEtudiant(Integer idEtudiant, Etudiant updateEtudiant) {
+        Etudiant etudiant = getEtudiant(idEtudiant);
+
+        etudiant.setNom(updateEtudiant.getNom());
+        etudiant.setPrenom(updateEtudiant.getPrenom());
+        etudiant.setCin(updateEtudiant.getCin());
+        etudiant.setSexe(updateEtudiant.getSexe());
+        etudiant.setTele(updateEtudiant.getTele());
+        etudiant.setDateNaissence(updateEtudiant.getDateNaissence());
+        etudiant.setLieuNaissence(updateEtudiant.getLieuNaissence());
+        etudiant.setCodeMassar(updateEtudiant.getCodeMassar());
+        etudiant.setSpecialite(updateEtudiant.getSpecialite());
+
+
+        Adresse newAdresse = updateEtudiant.getAdresse();
+        if (newAdresse!= null){
+            Optional<Adresse> existeAdresse = adresseRepository
+                    .findByRegionAndAndProvinceAndAndVille(
+                            newAdresse.getRegion(),
+                            newAdresse.getProvince(),
+                            newAdresse.getVille()
+                    );
+
+            if (existeAdresse.isPresent()){
+                etudiant.setAdresse(existeAdresse.get());
+            }else {
+                etudiant.setAdresse(newAdresse);
+            }
+        }
+
+        return etudiantRepository.save(etudiant);
     }
 }
