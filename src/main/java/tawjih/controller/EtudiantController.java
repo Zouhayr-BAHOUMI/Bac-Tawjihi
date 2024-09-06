4d9457043b67140a5505971321ff7e47502c718e
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-09-06 11:52:10 +0100

    make controller pour choisir un pack

--- src/main/java/tawjih/controller/EtudiantController.java
+++ src/main/java/tawjih/controller/EtudiantController.java
@@ -2,6 +2,7 @@ package tawjih.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import tawjih.model.Etudiant;
@@ -9,13 +10,13 @@ import tawjih.model.Pack;
 import tawjih.service.implimentation.EtudiantService;
 
 @RestController
-@RequestMapping("/etudiant/profile")
+@RequestMapping("/etudiant")
 public class EtudiantController {
 
     @Autowired
     private EtudiantService etudiantService;
 
-    @GetMapping
+    @GetMapping("/profile")
     public Etudiant showsEtudiantById(@AuthenticationPrincipal Etudiant etudiant){
        return etudiantService.getEtudiant(etudiant.getId());
     }
@@ -25,6 +26,15 @@ public class EtudiantController {
                                           @RequestBody Etudiant updatedEtudiant) {
 
         return etudiantService.updateEtudiant(etudiant.getId(), updatedEtudiant);
+
+    }
+
+    @PostMapping("/choisir-pack/{idPack}")
+    public ResponseEntity<Etudiant> selectPack(@AuthenticationPrincipal Etudiant etudiant,
+                                               @PathVariable Integer idPack) {
+
+        Etudiant etudiantLogged = etudiantService.choisirPack(etudiant.getId(),idPack);
+        return  ResponseEntity.ok(etudiantLogged);
 
     }
 }
