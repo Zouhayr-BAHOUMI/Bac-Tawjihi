a62a243f0ff8a71fa41790849eccfd29a25a409f
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-09-28 13:03:13 +0100

    faire add test and get tests in controller

--- src/main/java/tawjih/controller/TestController.java
+++ src/main/java/tawjih/controller/TestController.java
@@ -1,4 +1,40 @@
 package tawjih.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tawjih.model.Test;
+import tawjih.service.implimentation.TestService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/gestion-tests")
 public class TestController {
+
+    @Autowired
+    private TestService testService;
+
+
+    @PostMapping("/add")
+    public ResponseEntity<String> ajouterTest(@RequestBody Test test){
+        try {
+            testService.addTest(test);
+            return ResponseEntity.status(HttpStatus.CREATED).body("created successfully");
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("not created" + e.getMessage());
+        }
+    }
+
+    @GetMapping("/")
+    public List<Test> getAllTests() {
+        return testService.getAllTests();
+    }
+
+    @GetMapping("/idTest")
+    public Test showsTestById(@RequestParam Integer idTest){
+        return testService.getTest(idTest);
+    }
 }
