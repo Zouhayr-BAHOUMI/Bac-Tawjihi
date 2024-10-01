01c289e3a27bb899503360cb9f990ef305ba057d
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-10-01 16:52:16 +0100

    test unitaire pour register(si adding new admin its throw exception)

---
+++ src/test/java/tawjih/service/implimentation/AuthenticationServiceTest.java
@@ -0,0 +1,53 @@
+package tawjih.service.implimentation;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import tawjih.auth.RegisterRequest;
+import tawjih.config.JwtService;
+import tawjih.enums.Role;
+import tawjih.repository.PersonneRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthenticationServiceTest {
+
+
+    @Mock
+    private PersonneRepository personneRepository;
+
+    @Mock
+    private JwtService jwtService;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private AuthenticationManager authenticationManager;
+
+    @InjectMocks
+    private AuthenticationService authenticationService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+
+    @Test
+    public void Register_RoleIsAdmin() {
+        RegisterRequest request = new RegisterRequest();
+        Role role = Role.ADMIN;
+
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            authenticationService.register(request, role);
+        });
+
+        assertEquals("Cannot register new admin", exception.getMessage());
+    }
+
+}
