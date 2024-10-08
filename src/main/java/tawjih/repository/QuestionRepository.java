dc727a08f721d949dcb478ea748868886ecbcbc9
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-10-08 10:24:01 +0100

    faire repository pour fetches 10 random questions, and limits the result to 10 questions.

--- src/main/java/tawjih/repository/QuestionRepository.java
+++ src/main/java/tawjih/repository/QuestionRepository.java
@@ -1,10 +1,16 @@
 package tawjih.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import tawjih.model.Question;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Optional<Question> findByContenuQuestion(String contenuQuestion);
+
+    @Query(value = "SELECT * FROM question ORDER BY RAND() LIMIT :limit", nativeQuery = true)
+    List<Question> findRandomQuestions(@Param("limit") int limit);
 }
